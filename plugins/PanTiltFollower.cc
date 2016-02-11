3da8df6c9ae2bbf78be4a65fa320fe132a62b964
Thierry Carrard <thierry.carrard@orange.fr>
2016-02-11 20:43:21 +0100

    tracking done, exemple uses center of two simultanously tracked objets

--- plugins/PanTiltFollower.cc
+++ plugins/PanTiltFollower.cc
@@ -24,6 +24,7 @@ struct PanTiltFollower : public FleyePlugin
 		inline Vec2f(const Vec2f& v) : x(v.x) , y(v.y) {}
 
 		inline Vec2f operator + (Vec2f v) const { return Vec2f(x+v.x,y+v.y); }
+		inline Vec2f& operator += (Vec2f v) { *this = *this + v; return *this; }
 		inline Vec2f operator - (Vec2f v) const { return Vec2f(x-v.x,y-v.y); }
 		inline Vec2f operator * (float s) const { return Vec2f(x*s,y*s); }
 		inline Vec2f operator / (float s) const { return Vec2f(x/s,y/s); }
@@ -85,6 +86,8 @@ struct PanTiltFollower : public FleyePlugin
 	inline PanTiltFollower()
 		: m_ptsvc(0)
 		, m_txt(0)
+		, m_obj1(0)
+		, m_obj2(0)
 		, m_nci(0)
 		, m_ncj(0)
 		, m_npi(0)
@@ -105,7 +108,8 @@ struct PanTiltFollower : public FleyePlugin
 	void setup(FleyeContext* ctx)
 	{
 		m_ptsvc = PanTiltService_instance();
-		m_obj = TrackingService_instance()->getTrackedObject(0);
+		m_obj1 = TrackingService_instance()->getTrackedObject(0);
+		m_obj2 = TrackingService_instance()->getTrackedObject(1);
 		m_txt = TextService_instance()->addPositionnedText(0.1,0.2);
 		m_txt->out()<<"Initializing...";
 
@@ -124,7 +128,7 @@ struct PanTiltFollower : public FleyePlugin
 		m_ncj = layout["ControlGridY"].asInt();
 		m_npi = layout["ScreenGridX"].asInt();
 		m_npj = layout["ScreenGridY"].asInt();
-		std::cout<<"nCi="<<m_nci<<" nCj="<<m_ncj<<" nPi="<<m_npi<<" nPj="<<m_npj;
+		//std::cout<<"nCi="<<m_nci<<" nCj="<<m_ncj<<" nPi="<<m_npi<<" nPj="<<m_npj;
 		m_cgrid.resize( m_nci * m_ncj );
 		
 		Json::Value data = root["CalibrationData"];
@@ -135,14 +139,14 @@ struct PanTiltFollower : public FleyePlugin
 			int k = j * m_nci + i;
 			m_cgrid[k].C.x = cycle["Cx"].asDouble();
 			m_cgrid[k].C.y = cycle["Cy"].asDouble();
-			std::cout<<"cycle : Ci="<<i<<", j="<<j<<", Cx="<<m_cgrid[k].C.x<<", Cy="<<m_cgrid[k].C.y<<"\n";
+			//std::cout<<"cycle : Ci="<<i<<", j="<<j<<", Cx="<<m_cgrid[k].C.x<<", Cy="<<m_cgrid[k].C.y<<"\n";
 			m_cgrid[k].sgrid.resize( m_npi*m_npj );
 			Json::Value trackList = cycle["Samples"];
 			for( const Json::Value& track : trackList )
 			{
 				int Pi = track["Pi"].asInt();
 				int Pj = track["Pj"].asInt();
-				std::cout<<"track "<<Pi<<","<<Pj<<"\n";
+				//std::cout<<"track "<<Pi<<","<<Pj<<"\n";
 				int Pk = Pj * m_npi + Pi;
 				m_cgrid[k].sgrid[Pk].P.x = track["Px"].asDouble();
 				m_cgrid[k].sgrid[Pk].P.y = track["Py"].asDouble();
@@ -151,7 +155,7 @@ struct PanTiltFollower : public FleyePlugin
 				m_cgrid[k].sgrid[Pk].dPdCy.x = track["dPxdCy"].asDouble();
 				m_cgrid[k].sgrid[Pk].dPdCy.y = track["dPydCy"].asDouble();
 				m_cgrid[k].sgrid[Pk].normalize();
-				m_cgrid[k].sgrid[Pk].print();
+				//m_cgrid[k].sgrid[Pk].print();
 			}
 		}
 		
@@ -160,21 +164,27 @@ struct PanTiltFollower : public FleyePlugin
 		cobj->posX = 0.5;
 		cobj->posY = 0.5;
 
-		std::cout<<"PanTiltFollower ready : PanTiltService @"<<m_ptsvc<<", obj @"<<m_obj<< "\n";
+		std::cout<<"PanTiltFollower ready : PanTiltService @"<<m_ptsvc<<", obj1 @"<<m_obj1<<", obj2 @"<<m_obj2<< "\n";
 
 	}
 
 	void run(FleyeContext* ctx,int threadId)
 	{
+		// what is the target position of the tracked point
 		const Vec2f target( 0.5f, 0.5f );
 		
-		float W = m_obj->weight;
-		if( W < 1.e-9f ) return;
-		W=1.0f/W;
-		float px = m_obj->posX * W;
-		float py = m_obj->posY * W;
+		float W1 = m_obj1->weight;
+		float W2 = m_obj2->weight;
+		if( W1<1.0f || W2<1.0f) return;
+		W1=1.0f/W1;
+		W2=1.0f/W2;
+		Vec2f P1( m_obj1->posX * W1 , m_obj1->posY * W1 );
+		Vec2f P2( m_obj2->posX * W2 , m_obj2->posY * W2 );
 
-		Vec2f dP = target - Vec2f(px,py);
+		// this will be the position to track
+		Vec2f P = (P1+P2)*0.5f;
+
+		Vec2f dP = target - P;
 		if( dP.norm2() < 0.001 ) return;
 		
 		float cx = m_ptsvc->pan();
@@ -198,8 +208,8 @@ struct PanTiltFollower : public FleyePlugin
 		CalibrationSample c = interpolate( c1, c2, tcy );
 		
 		int pi=0,pj=0;
-		while( pi<(m_npi-2) && sgrid(c,pi,pj).P.x > px ) ++pi;
-		while( pj<(m_npj-2) && sgrid(c,pi,pj).P.y > py ) ++pj;
+		while( pi<(m_npi-2) && sgrid(c,pi,pj).P.x > P.x ) ++pi;
+		while( pj<(m_npj-2) && sgrid(c,pi,pj).P.y > P.y ) ++pj;
 		
 		// ensure this is an orthogonal grid
 		assert( sgrid(c,pi,pj).P.y == sgrid(c,pi+1,pj).P.y );
@@ -207,8 +217,8 @@ struct PanTiltFollower : public FleyePlugin
 		assert( sgrid(c,pi,pj).P.x == sgrid(c,pi,pj+1).P.x );
 		assert( sgrid(c,pi+1,pj).P.x == sgrid(c,pi+1,pj+1).P.x );
 
-		float tpx = ( px - sgrid(c,pi,pj).P.x ) / ( sgrid(c,pi+1,pj).P.x - sgrid(c,pi,pj).P.x );
-		float tpy = ( py - sgrid(c,pi,pj).P.y ) / ( sgrid(c,pi,pj+1).P.y - sgrid(c,pi,pj).P.y );
+		float tpx = ( P.x - sgrid(c,pi,pj).P.x ) / ( sgrid(c,pi+1,pj).P.x - sgrid(c,pi,pj).P.x );
+		float tpy = ( P.y - sgrid(c,pi,pj).P.y ) / ( sgrid(c,pi,pj+1).P.y - sgrid(c,pi,pj).P.y );
 
 		ScreenSample s1 = interpolate( sgrid(c,pi,pj) , sgrid(c,pi+1,pj) , tpx );
 		ScreenSample s2 = interpolate( sgrid(c,pi,pj+1) , sgrid(c,pi+1,pj+1) , tpx );
@@ -217,7 +227,10 @@ struct PanTiltFollower : public FleyePlugin
 		float nCx = dot( s.dPdCx , dP );
 		float nCy = dot( s.dPdCy , dP );
 		
-		m_txt->out() <<"nCx="<<nCx<<"\nnCy="<<nCy;
+		m_txt->out() <<"nCx="<<nCx
+					<<"\nnCy="<<nCy
+					<<"\nnCi="<<ci
+					<<"\nnCj="<<cj;
 
 		m_ptsvc->setPan( m_ptsvc->pan() -nCx*0.04 );
 		m_ptsvc->setTilt( m_ptsvc->tilt() -nCy*0.04 );
@@ -225,7 +238,8 @@ struct PanTiltFollower : public FleyePlugin
 
 	PanTiltService* m_ptsvc;
 	PositionnedText* m_txt;
-	TrackedObject* m_obj;
+	TrackedObject* m_obj1;
+	TrackedObject* m_obj2;
 	std::vector<CalibrationSample> m_cgrid;
 	int m_nci, m_ncj, m_npi, m_npj;
 };
